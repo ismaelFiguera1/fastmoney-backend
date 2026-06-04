d3592307a1a7c961f16a2bcf6099dd1ec43c5288
Taimol_rvz <94725506+Taimolrvz007@users.noreply.github.com>
2026-06-04 09:58:14 -0500

    auth de login agregado

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -7,11 +7,26 @@ export async function registerUser(nombre: string, email: string, password: stri
     throw new Error('El email ya está registrado');
   }
 
-  const hashContraseña = await bcrypt.hash(password, 10);
+  const hashContrasena = await bcrypt.hash(password, 10);
 
   const usuario = await prisma.user.create({
-    data: { nombre, email, hashContraseña },
+    data: { nombre, email, hashContrasena },
   });
+
+  return { id: usuario.id, nombre: usuario.nombre, email: usuario.email };
+}
+
+
+export async function loginUser(email: string, password: string) {
+  const usuario = await prisma.user.findUnique({ where: { email } });
+  if (!usuario) {
+    throw new Error('Usuario no encontrado');
+  }
+
+  const contraseñaValida = await bcrypt.compare(password, usuario.hashContrasena);
+  if (!contraseñaValida) {
+    throw new Error('Contraseña incorrecta');
+  }
 
   return { id: usuario.id, nombre: usuario.nombre, email: usuario.email };
 }
